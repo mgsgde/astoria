dc5ed7ce41550084fd70c3a797b117be962d3930
Magnus Goedde <mgsgde@gmail.com>
2023-12-10 15:01:14 +0100

    Edit next config.

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/astoria/webapp/out',
-  assetPrefix: '/astoria/webapp/out',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/astoria/webapp/out',
+  assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/astoria/webapp/out',
 };
 
 module.exports = nextConfig;
